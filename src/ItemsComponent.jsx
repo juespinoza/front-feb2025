790743c178f0612a8790fd95e20f3b842ee75899
Julia Espinoza <juespinozacorrea@gmail.com>
2025-03-10 20:21:54 -0300

    Clase 7 - 1

--- src/ItemsComponent.jsx
+++ src/ItemsComponent.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 
 function ItemsComponent({ items }) {
-  console.log(items);
+  // console.log(items);
   const [list, setList] = useState(items);
+  const [editingItem, setEditingItem] = useState({});
   // list.push({ monto: 1000, tipo: "ingreso" });
   // console.log(list);
   const esIngreso = (tipo) => tipo === "ingreso";
 
   const removeItemById = (id) => {
-    console.log("Borrando", id);
+    // console.log("Borrando", id);
     const newList = list.filter((elemento) => elemento.id !== id);
     setList(newList);
+  };
+
+  const handleSetEditingItem = (item) => {
+    console.log(item);
+    setEditingItem(item);
+  };
+
+  const handleSaveItem = (e) => {
+    e.preventDefault();
+    console.log("monto: ", e.target.monto.value);
+    const updatedList = list.map((item) =>
+      item.id === e.target.id.value
+        ? { ...item, monto: e.target.monto.value, tipo: e.target.tipo.value }
+        : item
+    );
+    setList(updatedList);
+    setEditingItem({});
   };
 
   return (
@@ -33,6 +51,9 @@ function ItemsComponent({ items }) {
                 className={`${esIngreso(item.tipo) ? "ingreso" : "egreso"}`}
               >
                 {`${esIngreso(item.tipo) ? "+" : "-"}` + item.monto}
+                <button onClick={() => handleSetEditingItem(item)}>
+                  Editar
+                </button>
                 <button onClick={() => removeItemById(item.id)}>Borrar</button>
               </li>
             );
@@ -46,31 +67,41 @@ function ItemsComponent({ items }) {
         <li className={`${esIngreso(items[2].tipo) ? "ingreso" : "egreso"}`}>
           {`${esIngreso(items[2].tipo) ? "+" : "-"}` + items[2].monto}
         </li> */}
-        <div>
-          <form>
-            <input type="number" id="monto" />
-            <select id="tipo">
-              <option value="ingreso">Ingreso</option>
-              <option value="egreso">Egreso</option>
-            </select>
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                const elemento = {
-                  id: Date.now(),
-                  monto: parseInt(e.target.form.monto.value),
-                  tipo: e.target.form.tipo.value,
-                };
-                // list.push(elemento); // ni items.push(elemento)
-                setList([...list, elemento]);
-                console.log("Agregando...", elemento);
-              }}
-            >
-              Agregar
-            </button>
-          </form>
-        </div>
       </ul>
+      <form id="editingItem" onSubmit={handleSaveItem}>
+        <input type="hidden" id="id" value={editingItem.id} />
+        <input type="number" id="monto" value={editingItem.monto} />
+        <select id="tipo" value={editingItem.tipo}>
+          <option value="ingreso">Ingreso</option>
+          <option value="egreso">Egreso</option>
+        </select>
+        <button type="submit">Guardar</button>
+        <button>Cancelar</button>
+      </form>
+      <div>
+        <form id="newItem">
+          <input type="number" id="monto" />
+          <select id="tipo">
+            <option value="ingreso">Ingreso</option>
+            <option value="egreso">Egreso</option>
+          </select>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              const elemento = {
+                id: Date.now(),
+                monto: parseInt(e.target.form.monto.value),
+                tipo: e.target.form.tipo.value,
+              };
+              // list.push(elemento); // ni items.push(elemento)
+              setList([...list, elemento]);
+              // console.log("Agregando...", elemento);
+            }}
+          >
+            Agregar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
