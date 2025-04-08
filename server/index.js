9ae7cf2417510d6eb44a67015027ec7b391d9fc3
Julia Espinoza <juespinozacorrea@gmail.com>
2025-04-07 21:13:11 -0300

    Clase 15 - Conf CORS, Nodemon, Dotenv. Creacion de endpoint PUT /usuarios/:id

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
 // 1- Importamos express
 const express = require("express");
 
+require("dotenv").config();
+
 // 2- Creamos la app
 const app = express();
 
 // 2.1- Activamos el middleware para leer JSON del req.body
 app.use(express.json());
 
+// 2.2- Activamos el middleware para activar la politica CORS
+const cors = require("cors");
+app.use(cors());
+
+// ------------------------
 // 3- Definimos una ruta básica, la ruta principal (/)
 app.get("/", (_req, res) => {
+  console.log(process.env.PORT);
   res.send("Hola mundo");
 });
 
@@ -28,6 +36,34 @@ app.post("/estudiante", (req, res) => {
   const { nombre, email } = req.body;
   res.json({ msg: "Dato recibido correctamente.", data: { nombre, email } });
 });
+// ------------------------
+
+// ------------------------ ENDPOINTS USUARIOS
+let usuarios = [];
+// GET /usuarios
+// GET /usuarios/:id
+// POST /usuarios
+
+// PUT /usuarios/:id
+app.put("/usuarios/:id", (req, res) => {
+  const { id } = req.params;
+  const { nombre, email } = req.body;
+  const usuario = usuarios.find((u) => u.id === Number(id));
+  if (!usuario) {
+    return res
+      .status(404)
+      .json({ mensaje: "Usuario no encontrado.", data: null });
+  }
+  usuarios.map((u) => {
+    if (u.id === Number(id)) {
+      u.nombre = nombre;
+      u.email = email;
+    }
+  });
+  res
+    .status(200)
+    .json({ mensaje: "Usuarios actualizado", data: { id, nombre, email } });
+});
 
 app.use((req, res) => {
   res.status(404).send("Ruta no disponible.");
@@ -35,7 +71,7 @@ app.use((req, res) => {
 
 // 4- Configuramos la escucha de un puerto y levantamos
 //    nuestro servidor
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`El servidor está corriendo en http://localhost:${PORT}`);
 });
