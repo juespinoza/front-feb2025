738c7f55b9cf5dc9e1308f7107820464f3bf2925
Julia Espinoza <juespinozacorrea@gmail.com>
2025-04-28 17:39:15 -0300

    Rutas de autenticacion modificadas

--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -7,12 +7,70 @@ require("dotenv").config();
 const userRouter = express.Router();
 
 // ------------------------ ENDPOINTS USUARIOS
-// {
-//   id: Number,
+// Usuario: {
+//   _id: Number,
 //   nombre: String,
-//   email: String
+//   email: String,
+//   password: String,
 // }
 let usuarios = [];
+
+userRouter.post("/register", async (req, res) => {
+  // console.log(req);
+  // console.log(req.body);
+  const { nombre, email, password } = req.body;
+  const usuarioExistente = usuarios.find((u) => u.email === email);
+  if (usuarioExistente) {
+    return res.status(400).json({ mensaje: "El usuario ya existe!" });
+  }
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const usuario = {
+    id: usuarios.length + 1, // esto es simulación
+    nombre,
+    email,
+    password: encryptedPassword,
+  };
+  usuarios.push(usuario);
+  res
+    .status(201)
+    .json({ mensaje: "Usuario creado correctamente", data: usuario });
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  // console.log({ email, password });
+  const usuario = usuarios.find((u) => u.email === email);
+  // console.log(usuarios);
+  // console.log(usuario);
+  if (!usuario) {
+    return res.status(404).json({ mensaje: "Usuario no encontrado" });
+  }
+  const passwordMatch = await bcrypt.compare(password, usuario.password);
+  // console.log(passwordMatch);
+  if (!passwordMatch) {
+    return res.status(401).json({ mensaje: "Contraseña incorrecta" });
+  }
+
+  const token = jwt.sign(
+    { id: usuario.id, email: usuario.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+  res.json({ mensaje: "Login existoso", token, data: usuario });
+});
+
+userRouter.get("/perfil", verificarToken, (req, res) => {
+  const { email } = req.body;
+  const usuario = usuarios.find((u) => u.email === email);
+  if (!usuario) {
+    return res.status(404).json({ mensaje: "Usuario no encontrado" });
+  }
+
+  res.json({ mensaje: "Perfil encontrado", data: usuario });
+});
+
 // GET /usuarios
 // Respuesta: todos los usuarios. res.json({mensaje: "", data: []})
 userRouter.get("/usuarios", (req, res) => {
@@ -92,62 +150,6 @@ userRouter.delete("/usuarios/:id", (req, res) => {
   // borrar en la base de datos (soft delete: borrado logico con columna)
   usuarios.splice(usuario, 1);
   res.json({ mensaje: "Usuario borrado correctamente." });
-});
-
-userRouter.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  // console.log({ email, password });
-  const usuario = usuarios.find((u) => u.email === email);
-  // console.log(usuarios);
-  // console.log(usuario);
-  if (!usuario) {
-    return res.status(404).json({ mensaje: "Usuario no encontrado" });
-  }
-  const passwordMatch = await bcrypt.compare(password, usuario.password);
-  // console.log(passwordMatch);
-  if (!passwordMatch) {
-    return res.status(401).json({ mensaje: "Contraseña incorrecta" });
-  }
-
-  const token = jwt.sign(
-    { id: usuario.id, email: usuario.email },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: "1h",
-    }
-  );
-  res.json({ mensaje: "Login existoso", token, data: usuario });
-});
-
-userRouter.get("/perfil", verificarToken, (req, res) => {
-  const { email } = req.body;
-  const usuario = usuarios.find((u) => u.email === email);
-  if (!usuario) {
-    return res.status(404).json({ mensaje: "Usuario no encontrado" });
-  }
-
-  res.json({ mensaje: "Perfil encontrado", data: usuario });
-});
-
-userRouter.post("/register", async (req, res) => {
-  // console.log(req);
-  // console.log(req.body);
-  const { nombre, email, password } = req.body;
-  const usuarioExistente = usuarios.find((u) => u.email === email);
-  if (usuarioExistente) {
-    return res.status(400).json({ mensaje: "El usuario ya existe!" });
-  }
-  const encryptedPassword = await bcrypt.hash(password, 10);
-  const usuario = {
-    id: usuarios.length + 1, // esto es simulación
-    nombre,
-    email,
-    password: encryptedPassword,
-  };
-  usuarios.push(usuario);
-  res
-    .status(201)
-    .json({ mensaje: "Usuario creado correctamente", data: usuario });
 });
 
 module.exports = userRouter;
